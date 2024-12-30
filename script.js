dc23aa0f5a1820e07377ac08dbe9728138035085
gangaramA <150507677+gangaramA@users.noreply.github.com>
2024-12-30 11:11:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,60 +1,28 @@
-//your JS code here. If required.
-const ip = document.getElementById('ip');
-const btn = document.getElementById('btn');
-const output = document.getElementById('output');
-
-btn.addEventListener('click', () => {
-  const inputNumber = Number(ip.value);
-
-  const promise1 = new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(inputNumber);
-      output.textContent = `Result: ${inputNumber}`; 
-    }, 2000); 
-  });
-
-  promise1
-    .then((num) => {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(num);
-          output.textContent = `Result: ${num}`; 
-        }, 2000);
-      });
-    })
-    .then((num) => { 
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(num * 2);
-          output.textContent = `Result: ${num * 2}`; 
-        }, 1000);
-      });
-    })
-    .then((num) => {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(num - 3);
-          output.textContent = `Result: ${num - 3}`; 
-        }, 1000);
-      });
-    })
-    .then((num) => {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(num / 2);
-          output.textContent = `Result: ${num / 2}`; 
-        }, 1000);
-      });
-    })
-    .then((num) => {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(num + 10);
-          output.textContent = `Final Result: ${num + 10}`; 
-        }, 1000);
-      });
-    })
-    .catch((error) => {
-      output.textContent = `Error: ${error}`;
-    });
-});
+() => { 
+  cy.visit(baseUrl + "/main.html"); 
+  cy.get("form"); 
+  
+  // Check Username
+  cy.get("label[for='username']").contains("Username:");
+  cy.get("input#username").should("be.empty");
+  
+  // Check Password
+  cy.get("label[for='password']").contains("Password:");
+  cy.get("input#password").should("be.empty");
+  
+  // Check Remember Me
+  cy.get("label[for='checkbox']").contains("Remember me");
+  cy.get("input#checkbox").should("not.be.checked");
+  
+  // Check Submit Button
+  cy.get("input#submit[value='Submit']").should("exist");
+  
+  // Check Existing User Button
+  cy.get("button#existing").should("not.be.visible");
+  
+  // Verify Local Storage
+  cy.window().then(win => { 
+    expect(win.localStorage.getItem("username")).to.be.null; 
+    expect(win.localStorage.getItem("password")).to.be.null; 
+  }); 
+}
